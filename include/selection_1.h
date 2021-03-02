7c242c4ac15e3bc5fbc73fc0ee108bd600b54df3
John Cleary <john@createk.io>
2021-03-02 10:57:56 +0000

    remove unused drink

--- include/selection_1.h
+++ include/selection_1.h
@@ -16,9 +16,6 @@ void loadDrinks()
   addDrink({"Vodka & Orange", {{vodka, 50}, {orange, 280}}});
   addDrink({"Margarita", {{tequila, 50}, {triple_sec, 20}, {lime_juice, 25}}});
   addDrink({"Barduino Hurricane", {{rum, 50}, {vodka, 50}, {lime_juice, 25}, {orange, 225}, {grenadine, 12}}});
-
-  //addDrink({"Cosmopolitan", {{lime_juice, 50}, {triple_sec, 15}, {cranberry, 30}});
-
   addDrink({"Orange Daiquir", {{rum, 60}, {orange, 20}, {grenadine, 10}, {lime_juice, 15}}});
   addDrink({"Tequila Sunrise", {{tequila, 50}, {orange, 100}, {grenadine, 30}, {lime_juice, 20}}});
 };
