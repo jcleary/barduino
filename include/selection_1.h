632deb01b516171e177670c5295ed9f3dd8075e8
John Cleary <john@createk.io>
2021-03-09 08:39:32 +0000

    add new cocktails

--- include/selection_1.h
+++ include/selection_1.h
@@ -13,11 +13,12 @@ const byte grenadine = 7;
 
 void loadDrinks()
 {
-  addDrink({.name = "Vodka & Orange",     .recipe = {{vodka, 50}, {orange, 280}}});
-  addDrink({.name = "Margarita",          .recipe = {{tequila, 50}, {triple_sec, 20}, {lime_juice, 25}}});
   addDrink({.name = "Barduino Hurricane", .recipe = {{rum, 50}, {vodka, 50}, {lime_juice, 25}, {orange, 225}}, .aftershot = {grenadine, 12}});
+  addDrink({.name = "Blood Test (shot)",  .recipe = {{tequila, 25}, {grenadine, 25}}});  
+  addDrink({.name = "Hairy Navel",        .recipe = {{vodka, 30}, {orange, 70}, {peach_schnapps, 50}}});
+  addDrink({.name = "Kamikaze (shot)",    .recipe = {{vodka, 25}, {triple_sec, 13}, {lime_juice, 12}}});
+  addDrink({.name = "Margarita",          .recipe = {{tequila, 50}, {triple_sec, 20}, {lime_juice, 25}}});
   addDrink({.name = "Orange Daiquir",     .recipe = {{rum, 60}, {orange, 20}, {lime_juice, 15}},               .aftershot = {grenadine, 10}});
-  addDrink({.name = "Tequila Sunrise",    .recipe = {{tequila, 50}, {orange, 100}, {lime_juice, 20}},          .aftershot = {grenadine, 30}});
   addDrink({.name = "Sex McDs Bathroom",  .recipe = {{vodka, 50}, {triple_sec, 50}, {peach_schnapps, 50}, {orange, 150}, {lime_juice, 25}}, .aftershot = {grenadine, 12}});
-  
+  addDrink({.name = "Tequila Sunrise",    .recipe = {{tequila, 50}, {orange, 100}, {lime_juice, 20}},          .aftershot = {grenadine, 30}});
 }
